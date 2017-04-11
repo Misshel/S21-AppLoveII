ce6b1e66835885cf995e7c448c0a746840e4c079
Misshel <jenmish094@gmail.com>
2017-04-10 20:46:29 -0500

    avance de modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,17 +6,29 @@ for(var i=1;i<13;i++){
   var img= document.createElement("img");
   var span= document.createElement("span");
   img.setAttribute("src","assets/image/img-"+ i +".jpg");
-//  span.innerHTML=i;
+  span.innerHTML=i;
   img.setAttribute("class","modal");
-  //div.appendChild(img);
-//  div.appendChild(span);
-  blockImage.appendChild(img);
+  div.setAttribute("class","div-modal")
+  div.appendChild(img);
+  div.appendChild(span);
+  blockImage.appendChild(div);
 }
 
-var modal=document.getElementsByClassName("modal");
+var modal= document.getElementsByClassName("div-modal");
 
-modal.addEventListener("click",modalFuntion);
+modal.addEventListener("click",funtion(event){
+  var img=document.getElementsByClassName("modal");
+  var close= document.createElement(Span);
+  close.setAttribute("id","cerrar");
 
-function modalFuntion(){
-
+  close.innerHTML="X";
+  modal.style.display = "block";
+  for(var i=1;i<13;i++){
+  img[i].src=this.src;
 }
+})
+var close= document.createElement(Span);
+
+close.addEventListener("click",funtion(){
+  modal.style.display = "none";
+})
